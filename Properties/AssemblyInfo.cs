144ad6f5a343ec49417a2ca1f385880758c2e196
youseemenot <sergei.gvianidze@gmail.com>
2026-05-05 23:38:10 +0300

    Add .gitignore and clean repository

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,21 +1,21 @@
-using System.Diagnostics;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Runtime.Versioning;
-using MelonLoader;
-using MinimapMod;
 
-[assembly: AssemblyTitle("MinimapMod F3")]
-[assembly: AssemblyDescription("")]
+// Основная информация о сборке
+[assembly: AssemblyTitle("Skoofidon's Minimap")]
+[assembly: AssemblyDescription("Advanced minimap and 3D radar mod for Schedule I")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MinimapMod F3")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyProduct("SkoofidonsMinimap")]
+[assembly: AssemblyCopyright("Copyright © Youseemenot 2026")]
 [assembly: AssemblyTrademark("")]
+
+// Параметр ComVisible со значением false делает типы в этой сборке невидимыми для COM-компонентов.
 [assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("12345678-1234-1234-1234-123456789012")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: MelonInfo(typeof(MainMod), "MinimapMod", "1.0.0", "Hiccup", null)]
-[assembly: MelonGame("TVGS", "Schedule I")]
-[assembly: AssemblyVersion("1.0.0.0")]
+
+// Сведения о версии сборки задаются версией нашего мода
+[assembly: AssemblyVersion("2.2.0.0")]
+[assembly: AssemblyFileVersion("2.2.0.0")]
